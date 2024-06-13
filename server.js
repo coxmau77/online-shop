1f6e9bf93b3e25c9d0420d117ed3a0f5e0c75c5c
Mauricio Cox <coxmau77@gmail.com>
2024-06-13 15:03:14 -0300

    fix: Configura el middleware para servir archivos estáticos

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 // console.info("Script que debe levantar un servidor");
 // servidor estatico en express
 const express = require('express');
+const path = require('node:path');
+
 const app = express();
 const PORT = 3000;
 
+// Configura el middleware para servir archivos estáticos
+// El contenido estático se servirá desde la carpeta "public"
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get("/", (request, response) => {
     // Enviar un documento al cliente
-    response.sendFile(__dirname + '/public/index.html');
+    response.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(PORT, () => {
