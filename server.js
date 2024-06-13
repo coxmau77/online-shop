a63fd4530aab7bd64e074e2b70eaab83c9fcf2b5
Mauricio Cox <coxmau77@gmail.com>
2024-06-13 14:35:48 -0300

    chore: creacion del servidor con express

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-console.info("Script que debe levantar un servidor");
+// console.info("Script que debe levantar un servidor");
+// servidor estatico en express
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get("/", (request, response) => {
+    // Esto se muestra al cliente
+    response.send("Script que debe levantar un servidor");
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor en ejecición  http://localhost:${PORT}`);
+})
