14c537670c54ab3c9479bcb0125c740c4a4c7aff
Mauricio Cox <coxmau77@gmail.com>
2024-06-21 16:17:31 -0300

    chore: Ruta POST funcionando

--- routes/user.route.js
+++ routes/user.route.js
@@ -2,6 +2,7 @@ console.log(">>> user route executed :) <<<");
 const express = require('express');
 const router = express.Router();
 
+// GET
 router.get('/', (request, response) => {
     // response.send("Ruta funcionando")
     // response.json({
@@ -28,13 +29,15 @@ router.get("/profile", (request, response) => {
     response.status(200).send("Perfil de usuario profile.html >")
 });
 
-// Esta ruta maneja las solicitudes POST a / signup y debería imprimir el contenido del cuerpo de la solicitud en la consola.
+// POST Esta ruta maneja las solicitudes POST a / signup y debería imprimir el contenido del cuerpo de la solicitud en la consola.
 router.post("/signup", (request, response) => {
 
-    let { titulo, precio } = request.body;
+    let { titulo, precioAR$ } = request.body;
 
     // Captura y muestra la informacón del cuerpo de la solicitud en la consola (Backend)
     console.info("Request, Requerir, Pedido, Solicitud todos son sinónimos para referirnos a lo mismo, obtengo la información que el cliente envía dentro del body >>> ", request.body);
+    console.log(titulo);
+    console.log(precioAR$);
 
     // Envía una respuesta al cliente
     // response.status(200).send(`>>>> REQUEST.BODY.TITULO ${titulo} >>>> REQUEST.BODY.PRECIO ${precio}`);
@@ -47,19 +50,10 @@ router.post("/signup", (request, response) => {
     );
 });
 
-// app.get("/contact", (request, response) => {
-//     // Enviar un documento al cliente
-//     response.sendFile(path.join(__dirname, 'public', 'contact.html'));
-// });
+// PUT
 
-// app.get("/signup", (request, response) => {
-//     // Enviar un documento al cliente
-//     response.sendFile(path.join(__dirname, 'public', 'signup.html'));
-// });
 
-// app.post("/signup", (request, response) => {
-//     // Enviar un documento al cliente
-//     response.send("Se envia en el body los datos del form a una BD");
-// });
+// DELETE
 
+/** Recordar siempre exportar el modulo creado */
 module.exports = router;
