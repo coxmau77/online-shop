616c3705ac7b348813b4f9106e89af2fced556e7
Mauricio Cox <coxmau77@gmail.com>
2024-07-08 16:21:19 -0300

    upload connection vercel

--- controller/product.controller.js
+++ controller/product.controller.js
@@ -1,82 +1,81 @@
 // /controller/product.controller.js
-document.addEventListener('DOMContentLoaded', () => {
 
-    console.log("/product.controller.js");
 
-    const dataBase = require('../db/bd');
+console.log("/product.controller.js");
 
-    const createProduct = (request, response) => {
-        const { sku, titulo, descripcion, precio } = request.body;
+const dataBase = require('../db/bd');
 
-        // Validación de los datos del producto
-        if (!sku || !titulo || !descripcion || !precio) {
-            return response.status(400).json({ message: "Todos los campos son obligatorios" });
+const createProduct = (request, response) => {
+    const { sku, titulo, descripcion, precio } = request.body;
+
+    // Validación de los datos del producto
+    if (!sku || !titulo || !descripcion || !precio) {
+        return response.status(400).json({ message: "Todos los campos son obligatorios" });
+    }
+    const SQL = 'INSERT INTO products (sku,titulo,descripcion,precio) VALUES (?,?,?,?)';
+
+    dataBase.query(SQL, [sku, titulo, descripcion, precio], (error, result) => {
+
+        if (error) throw error;
+
+        response.json({
+            message: "Producto creado con éxito",
+            idProduct: result.insertId
+        });
+    });
+};
+
+const getAll = (request, response) => {
+
+    console.log("Ruta que muestra todos los productos");
+    const SQL = 'SELECT * FROM products';
+
+    dataBase.query(SQL, (error, result) => {
+
+        // if (error) throw error;
+        if (error) {
+            console.error('Error al ejecutar la consulta:', error);
+            return response.status(500).json({ error: 'Error en el servidor' });
         }
-        const SQL = 'INSERT INTO products (sku,titulo,descripcion,precio) VALUES (?,?,?,?)';
 
-        dataBase.query(SQL, [sku, titulo, descripcion, precio], (error, result) => {
+        // if (result.length === 0) {
+        //     console.log('No hay usuarios en la lista');
+        //     return response.status(404).json({ mensaje: 'No existe ningún usuario aún' });
+        // }
 
-            if (error) throw error;
+        response.json(result)
+    });
+};
 
-            response.json({
-                message: "Producto creado con éxito",
-                idProduct: result.insertId
-            });
+const deleteProduct = (request, response) => {
+    const { id } = request.params;
+    const SQL = 'DELETE FROM products WHERE id = ?';
+
+    dataBase.query(SQL, [id], (error, result) => {
+        if (error) {
+            return response.status(500).json({ mensaje: 'Error NO EXISTE el producto', error });
+        }
+
+        response.json({
+            message: "EL productos se eliminó correctamente."
         });
-    };
+    });
+};
 
-    const getAll = (request, response) => {
+const uploadProduct = (request, response) => {
+    const { id } = request.params;
+    const { sku, titulo, descripcion, precio } = request.body;
 
-        console.log("Ruta que muestra todos los productos");
-        const SQL = 'SELECT * FROM products';
+    const SQL = 'UPDATE products SET sku = ?, titulo = ?, descripcion = ?, precio = ? WHERE id = ?';
+    dataBase.query(SQL, [sku, titulo, descripcion, precio, id], (error, result) => {
+        if (error) throw error;
 
-        dataBase.query(SQL, (error, result) => {
-
-            // if (error) throw error;
-            if (error) {
-                console.error('Error al ejecutar la consulta:', error);
-                return response.status(500).json({ error: 'Error en el servidor' });
-            }
-
-            // if (result.length === 0) {
-            //     console.log('No hay usuarios en la lista');
-            //     return response.status(404).json({ mensaje: 'No existe ningún usuario aún' });
-            // }
-
-            response.json(result)
+        response.json({
+            message: "Producto editado exitosamente!"
         });
-    };
+    });
+};
 
-    const deleteProduct = (request, response) => {
-        const { id } = request.params;
-        const SQL = 'DELETE FROM products WHERE id = ?';
-
-        dataBase.query(SQL, [id], (error, result) => {
-            if (error) {
-                return response.status(500).json({ mensaje: 'Error NO EXISTE el producto', error });
-            }
-
-            response.json({
-                message: "EL productos se eliminó correctamente."
-            });
-        });
-    };
-
-    const uploadProduct = (request, response) => {
-        const { id } = request.params;
-        const { sku, titulo, descripcion, precio } = request.body;
-
-        const SQL = 'UPDATE products SET sku = ?, titulo = ?, descripcion = ?, precio = ? WHERE id = ?';
-        dataBase.query(SQL, [sku, titulo, descripcion, precio, id], (error, result) => {
-            if (error) throw error;
-
-            response.json({
-                message: "Producto editado exitosamente!"
-            });
-        });
-    };
-
-    module.exports = { getAll, deleteProduct, uploadProduct, createProduct }
-})
+module.exports = { getAll, deleteProduct, uploadProduct, createProduct }
 
 
